48dcd683b3cb4df615d1b5b408b3ee4c7c8225fd
Brant Ashe <brantashe@gmail.com>
2024-06-08 13:53:44 -0500

    second attempt

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,9 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let basePath = '';
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
   output: isGithubActions ? 'export' : undefined,
-  basePath,
+  basePath: isGithubActions ? '/parallax-demo' : undefined,
 };
 
 export default nextConfig;
