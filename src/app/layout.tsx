06f5bbcf57c348b58e323608fcceb4bfd89507b6
Brant Ashe <brantashe@gmail.com>
2024-06-08 15:17:31 -0500

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,42 @@
 import prefix from '@/utils/prefix';
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import { ReactNode } from 'react';
 
 import './globals.scss';
+
+const neuePlak = localFont({
+  src: [
+    {
+      path: './NeuePlak-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './NeuePlak-Light.woff2',
+      weight: '300',
+      style: 'light',
+    },
+    {
+      path: './NeuePlak-Bold.woff2',
+      weight: '700',
+      style: 'bold',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: 'Will Ashe',
   description: 'Will Ashe - Software Engineer - Austin, TX',
 };
 
-export default function ParallaxDemoLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={neuePlak.className}>
       <body>
         <main
           style={{
